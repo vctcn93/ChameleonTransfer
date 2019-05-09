156f8a7d197353476480d01acfb9793bde156adf
Vctcn93 <vincentvane@yeah.net>
2019-05-09 18:26:09 +0800

    first commit

---
+++ chamelon/__init__.py
@@ -0,0 +1,3 @@
+from chameleon import constance
+from chameleon.transfer import *
+import chameleon.pandas_version as pdv
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+
+packages = find_packages()
+lc = open('LICENSE').read()
+rm = open('README.MD').read()
+
+setup(
+    name="pangolin.city",
+    version="1.0",
+    author="Vctcn93",
+    author_email="vincentvane@yeah.net",
+    description="实现 GIS 坐标系转换的基础包",
+    license=lc,
+    keywords="SCity",
+    packages=packages,
+    long_description=rm,
+    install_requires=['pandas', 'pytest'],
+)
